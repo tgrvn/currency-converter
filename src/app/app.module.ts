6753e87f617c99db11569486b7b14c505bdafbe2
tgrvn <warjllalka@gmail.com>
2022-12-03 18:22:04 +0200

    base ui done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ConverterComponent } from './components/converter/converter.component';
+import { DropdownComponent } from './components/dropdown/dropdown.component';
+import { TextInputComponent } from './components/text-input/text-input.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ConverterComponent,
+    DropdownComponent,
+    TextInputComponent
   ],
   imports: [
     BrowserModule
